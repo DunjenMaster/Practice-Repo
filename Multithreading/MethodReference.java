9695d3c7d35d458d0b55bc3ba9c018f63c6c33e3
Utkarsh <utkarshshukla2112@gmail.com>
2025-07-15 10:10:39 +0530

    MethodReference without static method.

--- Multithreading/MethodReference.java
+++ Multithreading/MethodReference.java
@@ -6,8 +6,8 @@ interface check {
 }
 
 public class MethodReference {
-
-    public static void reverse(String str)
+// withhout static mehod call the method reference in main class!
+    public void reverse(String str)
     {
         StringBuffer sb = new StringBuffer(str);
         sb.reverse();
@@ -16,7 +16,9 @@ public class MethodReference {
 
 
     public static void main(String[] args){
-        check ck = MethodReference::reverse;
+        // Using method reference to call the reverse method;  i.e. for non static method create the object and call from it.
+        MethodReference mr = new MethodReference();
+        check ck = mr::reverse;
         ck.checkMethod("Hello, this is a method reference example!");
     }
 
