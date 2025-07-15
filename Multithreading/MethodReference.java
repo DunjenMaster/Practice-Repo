d76748b046c4b15ba9a67152b3b6e192109e60f8
Utkarsh <utkarshshukla2112@gmail.com>
2025-07-15 10:26:04 +0530

    MethodReference with Constructor. Constructor of a class as reference to a method.

--- Multithreading/MethodReference.java
+++ Multithreading/MethodReference.java
@@ -6,19 +6,17 @@ interface check {
 }
 
 public class MethodReference {
-// withhout static mehod call the method reference in main class!
-    public void reverse(String str)
+// This is a method reference to the constructor of MethodReference class, this will print the string in uppercase. and be called as referenced inside main method.
+    public MethodReference(String s)
     {
-        StringBuffer sb = new StringBuffer(str);
-        sb.reverse();
-        System.out.println(sb);
+        System.out.println(s.toUpperCase());
     }
 
-
     public static void main(String[] args){
-        // Using method reference to call the reverse method;  i.e. for non static method create the object and call from it.
-        MethodReference mr = new MethodReference();
-        check ck = mr::reverse;
+// This is a method reference to the constructor of MethodReference class, this will print the string in uppercase.
+// Constructor is called with the help of new keyword and the interface is check is taking the reference of the constructor.
+
+        check ck = MethodReference::new;
         ck.checkMethod("Hello, this is a method reference example!");
     }
 
