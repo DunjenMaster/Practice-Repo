0af8577d6867c52156781c3fc76614f46c671ed9
Utkarsh <utkarshshukla2112@gmail.com>
2025-07-24 09:40:56 +0530

    FileInputStreamExample2.java : Learn how to read the file with FileInputStream.

---
+++ InputOutputStream/OutputStreams/FileOutputStreamExample2.java
@@ -0,0 +1,30 @@
+package InputOutputStream.OutputStreams;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileOutputStreamExample2
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            try(FileInputStream fis1 = new FileInputStream("D:/Project/Udemy/AbdulBari/LearningPractices/InputOutputStream/resources/test1.txt");)
+            {
+                byte b2[] = new byte[fis1.available()];
+               fis1.read(b2);
+
+               String str = new String(b2);
+                System.out.println("Data read from file: " + str);
+            }
+        }catch(FileNotFoundException e)
+        {
+            System.out.println(e);
+        }catch(IOException e)
+        {
+            System.out.println(e);
+        }
+    }
+}
