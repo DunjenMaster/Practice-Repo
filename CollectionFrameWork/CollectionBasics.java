47db62f5380791d9f0f90f80576a1c7c4c3090f6
Utkarsh <utkarshshukla2112@gmail.com>
2025-08-21 09:56:03 +0530

    Collection Interface generic methods for all classes.

---
+++ CollectionFrameWork/CollectionBasics.java
@@ -0,0 +1,37 @@
+/*
+Interface Collection : These are the generic methods which will nbe used in almost all the collection classes.
+1. add(E e) :
+2. addAll (Collection<E> c)
+3. remove(Object o)
+4. removeAll(Collection<E> c)
+5. retainAll(Collection<E> c)
+6. clearAll()
+7. isEmpty()
+8. contains(Object o)
+9. containsAll(Collection<E> c)
+10. equals(Object o)
+11. size()
+12. iterator()
+13. toArray()
+*/
+
+package CollectionFrameWork;
+
+public class CollectionBasics {
+    public static void main(String[] args) {
+        System.out.println("Interface Collection : These are the generic methods which will be used in almost all the collection classes.\n" +
+                "1. add(E e)\n" +
+                "2. addAll (Collection<E> c)\n" +
+                "3. remove(Object o)\n" +
+                "4. removeAll(Collection<E> c)\n" +
+                "5. retainAll(Collection<E> c)\n" +
+                "6. clearAll()\n" +
+                "7. isEmpty()\n" +
+                "8. contains(Object o)\n" +
+                "9. containsAll(Collection<E> c)\n" +
+                "10. equals(Object o)\n" +
+                "11. size()\n" +
+                "12. iterator()\n" +
+                "13. toArray()");
+    }
+}
