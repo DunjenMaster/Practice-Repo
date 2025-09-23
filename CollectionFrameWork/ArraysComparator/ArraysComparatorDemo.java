adb002673c949b14a3b486ad37011569c41e999f
Utkarsh <utkarshshukla2112@gmail.com>
2025-09-23 07:43:05 +0530

    Arrays: Add ArraysComparatorDemo to demonstrate array comparison

---
+++ CollectionFrameWork/ArraysComparator/ArraysComparatorDemo.java
@@ -0,0 +1,13 @@
+package CollectionFrameWork.ArraysComparator;
+
+import java.util.Arrays;
+
+public class ArraysComparatorDemo {
+    public static void main(String[] args){
+
+        int a[] = {2,4,6,8,1,3,5,7};
+        int b[] = {2,4,6,8,1,3,5,7};
+
+        System.out.println(Arrays.compare(a,b));
+    }
+}
